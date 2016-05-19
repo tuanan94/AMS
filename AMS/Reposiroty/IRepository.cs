537d36a4946cf2fa1ff3c27024fc278ca4445bcc
AnTTSE61258 <trantuanan@Trans-MacBook-Pro.local>
2016-05-19 14:25:15 +0700

    Repository

---
+++ AMS/Reposiroty/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMS.Reposiroty
+{
+    interface IRepository<T> where T : class
+    {
+        IEnumerable<T> List { get; }
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T FindById(object Id);
+    }
+}
---
+++ AMS/Service/TestService.cs
@@ -0,0 +1,17 @@
+﻿using AMS.Reposiroty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMS.Service
+{
+    public class TestService
+    {
+        GenericRepository<House> testRepository = new GenericRepository<House>();
+        public List<House> getAllHouse()
+        {
+            return testRepository.List.ToList();
+        }
+    }
+}
