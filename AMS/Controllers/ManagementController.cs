7e642f1942903638a2968d60511f01cb97789928
binhhtse61306 <binhhtse61306@fpt.edu.vn>
2016-05-19 12:54:33 +0700

    Management

---
+++ AMS/Controllers/ManagementController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMS.Controllers
+{
+    public class ManagementController : Controller
+    {
+        //
+        // GET: /Management/
+        public ActionResult AdminBoard()
+        {
+            return View();
+        }
+        public ActionResult ManageHousehold()
+        {
+            return View();
+        }
+
+        public ActionResult ManageRequest()
+        {
+            return View();
+        }
+        public ActionResult ManageIncome()
+        {
+            return View();
+        }
+        public ActionResult ManageReport()
+        {
+            return View();
+        }
+        public ActionResult ManageReipt()
+        {
+            return View();
+        }
+        public ActionResult ViewReceipt()
+        {
+            return View();
+        }
+        public ActionResult CreateReceipt()
+        {
+            return View();
+        }
+	}
+}
