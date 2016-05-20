8a203bc6e5dfb4341934d5efb21b0995a01ed322
binhhtse61306 <binhhtse61306@fpt.edu.vn>
2016-05-20 16:49:20 +0700

    commit

--- AMS/Controllers/TestController.cs
+++ AMS/Controllers/TestController.cs
@@ -9,6 +9,52 @@ using System.Web.Mvc;
 
 namespace AMS.Controllers
 {
+    //public class TestController : Controller
+    //{
+    //    TestService testService = new TestService();
+    //    // GET: Test
+    //    public ActionResult Index()
+    //    {
+    //        List<House> allHouse = testService.getAllHouse();
+    //        ViewBag.allHouse = allHouse;
+    //        return View();
+    //    }
+    //    [HttpPost]
+    //    [ValidateInput(false)]
+    //    public ActionResult addHome(String Block, String Floor, String HouseName)
+    //    {
+    //        bool isValid = true;
+    //        isValid = !HouseName.Equals("");
+    //        //Step 1: Valid
+    //        if (isValid)
+    //        {
+    //            //Step 2: Send to service to do business
+    //            testService.addHouse(Block, Floor, HouseName);
+    //        }
+
+    //        return RedirectToAction("Index");
+    //    }
+    //    [HttpGet]
+    //    public Object allHouse()
+    //    {
+    //        return JsonConvert.SerializeObject(testService.getAllHouse());
+
+    //    }
+    //    [HttpPost]
+    //    public void addHouseAjax(String Block, String Floor, String HouseName)
+    //    {
+    //        bool isValid = true;
+    //        //Step 1: Valid
+    //        isValid = !HouseName.Equals("");
+    //        if (isValid)
+    //        {
+    //            //Step 2: Send to service to do business
+    //            testService.addHouse(Block, Floor, HouseName);
+    //        }
+    //    }
+    //}
+//namespace AMS.Controllers
+//{
     public class TestController : Controller
     {
         TestService testService = new TestService();
@@ -37,7 +83,10 @@ namespace AMS.Controllers
         [HttpGet]
         public Object allHouse()
         {
-            return JsonConvert.SerializeObject(testService.getAllHouse());
+            return JsonConvert.SerializeObject(testService.getAllHouse(), Formatting.Indented, new JsonSerializerSettings
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects
+            });
 
         }
         [HttpPost]
@@ -52,56 +101,7 @@ namespace AMS.Controllers
                 testService.addHouse(Block, Floor, HouseName);
             }
         }
+
     }
-//namespace AMS.Controllers
-//{
-//    public class TestController : Controller
-//    {
-//        TestService testService = new TestService();
-//        // GET: Test
-//        public ActionResult Index()
-//        {
-//            List<House> allHouse = testService.getAllHouse();
-//            ViewBag.allHouse = allHouse;
-//            return View();
-//        }
-//        [HttpPost]
-//        [ValidateInput(false)]
-//        public ActionResult addHome(String Block, String Floor, String HouseName)
-//        {
-//            bool isValid = true;
-//            isValid = !HouseName.Equals("");
-//            //Step 1: Valid
-//            if (isValid)
-//            {
-//                //Step 2: Send to service to do business
-//                testService.addHouse(Block, Floor, HouseName);
-//            }
-
-//            return RedirectToAction("Index");
-//        }
-//        [HttpGet]
-//        public Object allHouse()
-//        {
-//            return JsonConvert.SerializeObject(testService.getAllHouse(),Formatting.Indented, new JsonSerializerSettings
-//            {
-//                PreserveReferencesHandling = PreserveReferencesHandling.Objects
-//            });
-
-//        }
-//        [HttpPost]
-//        public void addHouseAjax(String Block, String Floor, String HouseName)
-//        {
-//            bool isValid = true;
-//            //Step 1: Valid
-//            isValid = !HouseName.Equals("");
-//            if (isValid)
-//            {
-//                //Step 2: Send to service to do business
-//                testService.addHouse(Block, Floor, HouseName);
-//            }
-//        }
-
-//    }
 
 }
