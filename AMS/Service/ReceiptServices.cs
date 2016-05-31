f9353a5b97b0e85314d2fe3133bbb7faf806d526
anltnmse60906 <anltnmse60906@fpt.edu.vn>
2016-05-31 11:26:38 +0700

    Resident views history order
    Resident views history order

--- AMS/Service/ReceiptServices.cs
+++ AMS/Service/ReceiptServices.cs
@@ -9,10 +9,19 @@ namespace AMS.Service
     public class ReceiptServices
     {
         public GenericRepository<Receipt> _receiptRepository = new GenericRepository<Receipt>();
-        public void Add (Receipt receipt)
-        {                                                        
-            _receiptRepository.Add(receipt);                        
+        public void Add(Receipt receipt)
+        {
+            _receiptRepository.Add(receipt);
         }
+        public List<Receipt> FindByHouseId(int houseId)
+        {
+            return _receiptRepository.List.Where(r => r.HouseId == houseId && r.Status != SLIM_CONFIG.RECEIPT_STATUS_UNPUBLISHED).OrderByDescending(r => r.CreateDate).ToList();
+        }
+        public Receipt FindById(int id)
+        {
+            return _receiptRepository.List.Where(r => r.Id == id).First();
+        }
+
     }
     public class ReceiptDetailServices
     {
